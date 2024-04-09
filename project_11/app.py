5b19a1d1a385d901f45a821dd5b40e969cd48ea2
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-04-09 16:38:32 -0400

    added updated code

--- project_11/app.py
+++ project_11/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, send_from_directory
+import json
 
 app = Flask(__name__,static_url_path='')
 
+with open("artwork.json","r") as f:
+    artwork = json.load(f)
+
 @app.route('/')
 def send_page():
-    return render_template("chapter08-project01.html")
+    return render_template("chapter08-project01.html",paintings=artwork)
 
 
 if __name__ == '__main__':
--- project_11/static/js/functions.js
+++ project_11/static/js/functions.js
@@ -1,1 +1,9 @@
 /* add in your functions here */
+
+function update_price(id) {
+    var element = document.getElementById(id);
+    var cells = element.children;
+    var amount = parseInt(cells[2].children[0].value);
+    var cost = parseFloat(cells[3].innerHTML);
+    cells[4].innerHTML = "$"+(cost * amount).toFixed(2);
+}
